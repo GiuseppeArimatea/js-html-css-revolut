156677114be4babfeac9b5d42720928c9c8e7f61
giuseppe <Giuseppearimatea10@gmail.com>
2020-01-21 10:44:11 +0100

    commit

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
 
 
-$(document).ready(
-  function () {
+$(document).ready(function () {
     $('ul > li.clicca').mouseenter(
       function () {
-        $(this).children('.dropdown').show();
+        $(this).children('.dropdown').addClass('active');
+        $(this).find('i.fa-chevron-down').removeClass('fa-chevron-down').addClass('fa-chevron-up');
       }
     );
     $('ul > li.clicca').mouseleave(
       function () {
-         $(this).children('.dropdown').hide();
+         $(this).children('.dropdown').removeClass('active');
+         $(this).find('i.fa-chevron-up').removeClass('fa-chevron-up').addClass('fa-chevron-down');
        }
-     );
+     )
   }
 );
